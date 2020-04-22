f51f311c93c6159433691be5bd0d1627fb54520b
Matthew Pickering <matthewtpickering@gmail.com>
2020-04-22 20:54:25 +0100

    Implement Goto Type Definition

--- test/exe/Main.hs
+++ test/exe/Main.hs
@@ -1353,7 +1353,11 @@ findDefinitionAndHoverTests = let
 
   mkFindTests tests = testGroup "get"
     [ testGroup "definition" $ mapMaybe fst tests
-    , testGroup "hover"      $ mapMaybe snd tests ]
+    , testGroup "hover"      $ mapMaybe snd tests
+    , testGroup "type-definition" $ typeDefinitionTests ]
+
+  typeDefinitionTests = [ tst (getTypeDefinitions, checkDefs) dcL7 tcData "Saturated data con"
+                        , tst (getTypeDefinitions, checkDefs) opL16 [ExpectNoDefinitions] "Polymorphic variable"]
 
   test runDef runHover look expect = testM runDef runHover look (return expect)
 
@@ -1362,7 +1366,6 @@ findDefinitionAndHoverTests = let
     , runHover $ tst hover look expect title ) where
       def   = (getDefinitions, checkDefs)
       hover = (getHover      , checkHover)
-      --type_ = (getTypeDefinitions, checkTDefs) -- getTypeDefinitions always times out
 
   -- search locations            expectations on results
   fffL4  = _start fffR     ;  fffR = mkRange 8  4    8  7 ; fff  = [ExpectRange fffR]
