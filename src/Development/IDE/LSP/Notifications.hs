40aeb99a424c3b8ea14fa438707bca3633e002fb
Matthew Pickering <matthewtpickering@gmail.com>
2020-04-25 10:39:40 +0100

    TypeCheck files when they are opened or modified
    Do NOT kick
    
    Kicking was an action triggered on every database run which added approx
    0.2s to the time taken to hover for no benefit. Now kicking is
    controlled in a more fine-grained way directly by triggering a typecheck
    when a file is modified.

--- src/Development/IDE/LSP/Notifications.hs
+++ src/Development/IDE/LSP/Notifications.hs
@@ -24,7 +24,7 @@ import           Data.Maybe
 import qualified Data.HashSet                     as S
 import qualified Data.Text                        as Text
 
-import           Development.IDE.Core.FileStore   (setSomethingModified)
+import           Development.IDE.Core.FileStore   (setSomethingModified, setFileModified)
 import           Development.IDE.Core.FileExists  (modifyFileExists)
 import           Development.IDE.Core.OfInterest
 
@@ -44,12 +44,12 @@ setHandlersNotifications = PartialHandlers $ \WithMessage{..} x -> return x
     ,LSP.didChangeTextDocumentNotificationHandler = withNotification (LSP.didChangeTextDocumentNotificationHandler x) $
         \_ ide (DidChangeTextDocumentParams identifier@VersionedTextDocumentIdentifier{_uri} changes) -> do
             updatePositionMapping ide identifier changes
-            setSomethingModified ide
+            whenUriFile _uri $ \file -> setFileModified ide file
             logInfo (ideLogger ide) $ "Modified text document: " <> getUri _uri
 
     ,LSP.didSaveTextDocumentNotificationHandler = withNotification (LSP.didSaveTextDocumentNotificationHandler x) $
         \_ ide (DidSaveTextDocumentParams TextDocumentIdentifier{_uri}) -> do
-            setSomethingModified ide
+            whenUriFile _uri $ \file -> setFileModified ide file
             logInfo (ideLogger ide) $ "Saved text document: " <> getUri _uri
 
     ,LSP.didCloseTextDocumentNotificationHandler = withNotification (LSP.didCloseTextDocumentNotificationHandler x) $
