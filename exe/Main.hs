2db84e49916a97222f155562a172853d075a1f42
Matthew Pickering <matthewtpickering@gmail.com>
2020-04-30 08:20:02 +0100

    Changes to exe/Main.hs

--- exe/Main.hs
+++ exe/Main.hs
@@ -165,8 +165,9 @@ main = do
 
         putStrLn "\nStep 4/6: Type checking the files"
         setFilesOfInterest ide $ HashSet.fromList $ map toNormalizedFilePath' files
---        _ <- runActionSync "TypecheckTest" ide $ uses TypeCheck (map toNormalizedFilePath' files)
+        _ <- runActionSync "TypecheckTest" ide $ uses TypeCheck (map toNormalizedFilePath' files)
 --        results <- runActionSync ide $ use TypeCheck $ toNormalizedFilePath' "src/Development/IDE/Core/Rules.hs"
+        {-
         let fp =  toNormalizedFilePath' "ghc/Main.hs"
         results <- runActionSync "tc" ide $ use TypeCheck $ toNormalizedFilePath' "ghc/Main.hs"
         hover1 <- duration $ runIdeAction "Hover" ide $ getAtPoint fp (Position 950 20)
@@ -175,6 +176,7 @@ main = do
         hover2 <- duration $ runIdeAction "Hover" ide $ getAtPoint fp (Position 950 20)
         print hover2
         traceMarkerIO "END"
+        -}
         cancel worker
         return ()
 
