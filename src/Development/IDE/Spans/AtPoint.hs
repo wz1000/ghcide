7c2c36fb173c06e3a916b6de60ddea8a38af2634
Zubin Duggal <zubin@cmi.ac.in>
2020-05-04 20:29:43 +0100

    Reimplement Hover/GotoDefn in terms of Hie Files
    Co-authored-by: Matthew Pickering <matthewtpickering@gmail.com>

--- src/Development/IDE/Spans/AtPoint.hs
+++ src/Development/IDE/Spans/AtPoint.hs
@@ -12,12 +12,12 @@ module Development.IDE.Spans.AtPoint (
 import           Development.IDE.GHC.Error
 import Development.IDE.GHC.Orphans()
 import Development.IDE.Types.Location
+import           Language.Haskell.LSP.Types
 
 -- DAML compiler and infrastructure
 import Development.IDE.GHC.Compat
 import Development.IDE.Types.Options
-import Development.IDE.Spans.Type as SpanInfo
-import Development.IDE.Spans.Common (spanDocToMarkdown)
+import Development.IDE.Spans.Common
 
 -- GHC API imports
 import DynFlags
@@ -25,8 +25,6 @@ import FastString
 import Name
 import Outputable hiding ((<>))
 import SrcLoc
-import Type
-import VarSet
 
 import Control.Monad.Extra
 import Control.Monad.Trans.Maybe
@@ -34,12 +32,16 @@ import Control.Monad.IO.Class
 import           Data.Maybe
 import           Data.List
 import qualified Data.Text as T
+import qualified Data.Map as M
+import qualified Data.Array as A
 
+import IfaceType
+import Data.Either
 gotoTypeDefinition
   :: MonadIO m
   => (Module -> MaybeT m (HieFile, FilePath))
   -> IdeOptions
-  -> [SpanInfo]
+  -> HieFile
   -> Position
   -> MaybeT m Location
 gotoTypeDefinition getHieFile ideOpts srcSpans pos
@@ -50,7 +52,7 @@ gotoDefinition
   :: MonadIO m
   => (Module -> MaybeT m (HieFile, FilePath))
   -> IdeOptions
-  -> [SpanInfo]
+  -> HieFile
   -> Position
   -> MaybeT m Location
 gotoDefinition getHieFile ideOpts srcSpans pos =
@@ -59,75 +61,37 @@ gotoDefinition getHieFile ideOpts srcSpans pos =
 -- | Synopsis for the name at a given position.
 atPoint
   :: IdeOptions
-  -> SpansInfo
+  -> HieFile
+  -> DocMap
   -> Position
   -> Maybe (Maybe Range, [T.Text])
-atPoint IdeOptions{..} (SpansInfo srcSpans cntsSpans) pos = do
-    firstSpan <- listToMaybe $ deEmpasizeGeneratedEqShow $ spansAtPoint pos srcSpans
-    let constraintsAtPoint = mapMaybe spaninfoType (spansAtPoint pos cntsSpans)
-    return (Just (range firstSpan), hoverInfo firstSpan constraintsAtPoint)
+atPoint IdeOptions{} hf dm pos = listToMaybe $ pointCommand hf pos hoverInfo
   where
-    -- Hover info for types, classes, type variables
-    hoverInfo SpanInfo{spaninfoType = Nothing , spaninfoDocs = docs ,  ..} _ =
-       (wrapLanguageSyntax <$> name) <> location <> spanDocToMarkdown docs
-     where
-       name     = [maybe shouldNotHappen showName  mbName]
-       location = [maybe shouldNotHappen definedAt mbName]
-       shouldNotHappen = "ghcide: did not expect a type level component without a name"
-       mbName = getNameM spaninfoSource
-
     -- Hover info for values/data
-    hoverInfo SpanInfo{spaninfoType = (Just typ), spaninfoDocs = docs , ..} cnts =
-       (wrapLanguageSyntax <$> nameOrSource) <> location <> spanDocToMarkdown docs
-     where
-       mbName = getNameM spaninfoSource
-       expr = case spaninfoSource of
-                Named n -> qualifyNameIfPossible n
-                Lit   l -> crop $ T.pack l
-                _       -> ""
-       nameOrSource   = [expr <> "\n" <> typeAnnotation]
-       qualifyNameIfPossible name' = modulePrefix <> showName name'
-         where modulePrefix = maybe "" (<> ".") (getModuleNameAsText name')
-       location = [maybe "" definedAt mbName]
+    hoverInfo ast =
+      (Just range, prettyNames ++ map wrapHaskell prettyTypes)
+      where
+        range = srcSpanToRange (RealSrcSpan $ nodeSpan ast)
 
-       thisFVs = tyCoVarsOfType typ
-       constraintsOverFVs = filter (\cnt -> not (tyCoVarsOfType cnt `disjointVarSet` thisFVs)) cnts
-       constraintsT = T.intercalate ", " (map showName constraintsOverFVs)
+        wrapHaskell x = "\n```haskell\n"<>x<>"\n```\n"
+        info = nodeInfo ast
+        names = M.assocs $ nodeIdentifiers info
+        types = nodeType info
 
-       typeAnnotation = case constraintsOverFVs of
-                          []  -> colon <> showName typ
-                          [_] -> colon <> constraintsT <> "\n=> " <> showName typ
-                          _   -> colon <> "(" <> constraintsT <> ")\n=> " <> showName typ
+        prettyNames :: [T.Text]
+        prettyNames = map prettyName names
+        prettyName (Right n, dets) = T.unlines $
+          wrapHaskell (showName n <> maybe "" (" :: " <> ) (prettyType <$> identType dets))
+          : definedAt n
+          : concat (maybeToList (spanDocToMarkdown <$> M.lookup n dm))
+        prettyName (Left m,_) = showName m
 
-    definedAt name = "*Defined " <> T.pack (showSDocUnsafe $ pprNameDefnLoc name) <> "*\n"
+        prettyTypes = map (("_ :: "<>) . prettyType) types
+        prettyType t = showName $ hieTypeToIface $ recoverFullType t arr
 
-    crop txt
-      | T.length txt > 50 = T.take 46 txt <> " ..."
-      | otherwise         = txt
+        definedAt name = "*Defined " <> T.pack (showSDocUnsafe $ pprNameDefnLoc name) <> "*"
 
-    range SpanInfo{..} = Range
-      (Position spaninfoStartLine spaninfoStartCol)
-      (Position spaninfoEndLine spaninfoEndCol)
-
-    colon = if optNewColonConvention then ": " else ":: "
-    wrapLanguageSyntax x = T.unlines [ "```" <> T.pack optLanguageSyntax, x, "```"]
-
-    -- NOTE(RJR): This is a bit hacky.
-    -- We don't want to show the user type signatures generated from Eq and Show
-    -- instances, as they do not appear in the source program.
-    -- However the user could have written an `==` or `show` function directly,
-    -- in which case we still want to show information for that.
-    -- Hence we just move such information later in the list of spans.
-    deEmpasizeGeneratedEqShow :: [SpanInfo] -> [SpanInfo]
-    deEmpasizeGeneratedEqShow = uncurry (++) . partition (not . isTypeclassDeclSpan)
-    isTypeclassDeclSpan :: SpanInfo -> Bool
-    isTypeclassDeclSpan spanInfo =
-      case getNameM (spaninfoSource spanInfo) of
-        Just name -> any (`isInfixOf` getOccString name) ["==", "showsPrec"]
-        Nothing -> False
-
-
-
+        arr = hie_types hf
 
 typeLocationsAtPoint
   :: forall m
@@ -135,15 +99,17 @@ typeLocationsAtPoint
   => (Module -> MaybeT m (HieFile, FilePath))
   -> IdeOptions
   -> Position
-  -> [SpanInfo]
+  -> HieFile
   -> m [Location]
-typeLocationsAtPoint getHieFile = querySpanInfoAt getTypeSpan
-  where getTypeSpan :: SpanInfo -> m (Maybe SrcSpan)
-        getTypeSpan SpanInfo { spaninfoType = Just t } =
-          case splitTyConApp_maybe t of
-            Nothing -> return Nothing
-            Just (getName -> name, _) -> nameToLocation getHieFile name
-        getTypeSpan _ = return Nothing
+typeLocationsAtPoint getHieFile _ideOptions pos ast =
+  let ts = concat $ pointCommand ast pos (nodeType . nodeInfo)
+      arr = hie_types ast
+      its = map (arr A.!) ts
+      ns = flip mapMaybe its $ \case
+        HTyConApp tc _ -> Just $ ifaceTyConName tc
+        HTyVarTy n -> Just n
+        _ -> Nothing
+    in mapMaybeM (nameToLocation getHieFile) ns
 
 locationsAtPoint
   :: forall m
@@ -151,28 +117,15 @@ locationsAtPoint
   => (Module -> MaybeT m (HieFile, FilePath))
   -> IdeOptions
   -> Position
-  -> [SpanInfo]
+  -> HieFile
   -> m [Location]
-locationsAtPoint getHieFile = querySpanInfoAt (getSpan . spaninfoSource)
-  where getSpan :: SpanSource -> m (Maybe SrcSpan)
-        getSpan NoSource = pure Nothing
-        getSpan (SpanS sp) = pure $ Just sp
-        getSpan (Lit _) = pure Nothing
-        getSpan (Named name) = nameToLocation getHieFile name
-
-querySpanInfoAt :: forall m
-   . MonadIO m
-  => (SpanInfo -> m (Maybe SrcSpan))
-  -> IdeOptions
-  -> Position
-  -> [SpanInfo]
-  -> m [Location]
-querySpanInfoAt getSpan _ideOptions pos =
-    fmap (map srcSpanToLocation) . mapMaybeM getSpan . spansAtPoint pos
+locationsAtPoint getHieFile _ideOptions pos ast =
+  let ns = concat $ pointCommand ast pos (rights . M.keys . nodeIdentifiers . nodeInfo)
+    in mapMaybeM (nameToLocation getHieFile) ns
 
 -- | Given a 'Name' attempt to find the location where it is defined.
-nameToLocation :: Monad f => (Module -> MaybeT f (HieFile, String)) -> Name -> f (Maybe SrcSpan)
-nameToLocation getHieFile name =
+nameToLocation :: Monad f => (Module -> MaybeT f (HieFile, String)) -> Name -> f (Maybe Location)
+nameToLocation getHieFile name = fmap (fmap srcSpanToLocation) $
               case nameSrcSpan name of
                 sp@(RealSrcSpan _) -> pure $ Just sp
                 sp@(UnhelpfulSpan _) -> runMaybeT $ do
@@ -195,21 +148,17 @@ nameToLocation getHieFile name =
     setFileName f (RealSrcSpan span) = RealSrcSpan (span { srcSpanFile = mkFastString f })
     setFileName _ span@(UnhelpfulSpan _) = span
 
--- | Filter out spans which do not enclose a given point
-spansAtPoint :: Position -> [SpanInfo] -> [SpanInfo]
-spansAtPoint pos = filter atp where
-  line = _line pos
-  cha = _character pos
-  atp SpanInfo{..} =
-      startsBeforePosition && endsAfterPosition
-    where
-      startLineCmp = compare spaninfoStartLine line
-      endLineCmp   = compare spaninfoEndLine   line
-
-      startsBeforePosition = startLineCmp == LT || (startLineCmp == EQ && spaninfoStartCol <= cha)
-                                              -- The end col points to the column after the
-                                              -- last character so we use > instead of >=
-      endsAfterPosition = endLineCmp == GT || (endLineCmp == EQ && spaninfoEndCol > cha)
+pointCommand :: HieFile -> Position -> (HieAST TypeIndex -> a) -> [a]
+pointCommand hf pos k =
+    catMaybes $ M.elems $ flip M.mapWithKey (getAsts $ hie_asts hf) $ \fs ast ->
+      case selectSmallestContaining (sp fs) ast of
+        Nothing -> Nothing
+        Just ast' -> Just $ k ast'
+ where
+   sloc fs = mkRealSrcLoc fs (line+1) (cha+1)
+   sp fs = mkRealSrcSpan (sloc fs) (sloc fs)
+   line = _line pos
+   cha = _character pos
 
 showName :: Outputable a => a -> T.Text
 showName = T.pack . prettyprint
@@ -217,7 +166,3 @@ showName = T.pack . prettyprint
     prettyprint x = renderWithStyle unsafeGlobalDynFlags (ppr x) style
     style = mkUserStyle unsafeGlobalDynFlags neverQualify AllTheWay
 
-getModuleNameAsText :: Name -> Maybe T.Text
-getModuleNameAsText n = do
-  m <- nameModule_maybe n
-  return . T.pack . moduleNameString $ moduleName m
