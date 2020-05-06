59eb64e3b106bda3477318b72af1ed8285263e2f
Zubin Duggal <zubin@cmi.ac.in>
2020-05-06 22:02:27 +0530

    Don't report invalid positions, introduce safe versions of srcSpanToRange and srcSpanToLocation

--- src/Development/IDE/GHC/Error.hs
+++ src/Development/IDE/GHC/Error.hs
@@ -13,7 +13,10 @@ module Development.IDE.GHC.Error
 
   -- * utilities working with spans
   , srcSpanToLocation
+  , srcSpanToLocationMaybe
   , srcSpanToRange
+  , srcSpanToRangeMaybe
+  , realSrcSpanToRange
   , srcSpanToFilename
   , zeroSpan
   , realSpan
@@ -89,6 +92,10 @@ srcSpanToRange :: SrcSpan -> Range
 srcSpanToRange (UnhelpfulSpan _)  = noRange
 srcSpanToRange (RealSrcSpan real) = realSrcSpanToRange real
 
+srcSpanToRangeMaybe :: SrcSpan -> Maybe Range
+srcSpanToRangeMaybe (UnhelpfulSpan _)  = Nothing
+srcSpanToRangeMaybe (RealSrcSpan real) = Just $ realSrcSpanToRange real
+
 realSrcSpanToRange :: RealSrcSpan -> Range
 realSrcSpanToRange real =
   Range (Position (srcSpanStartLine real - 1) (srcSpanStartCol real - 1))
@@ -104,6 +111,10 @@ srcSpanToLocation :: SrcSpan -> Location
 srcSpanToLocation src =
   -- important that the URI's we produce have been properly normalized, otherwise they point at weird places in VS Code
   Location (fromNormalizedUri $ filePathToUri' $ toNormalizedFilePath' $ srcSpanToFilename src) (srcSpanToRange src)
+
+srcSpanToLocationMaybe :: SrcSpan -> Maybe Location
+srcSpanToLocationMaybe (UnhelpfulSpan _) = Nothing
+srcSpanToLocationMaybe src = Just $ srcSpanToLocation src
 
 isInsideSrcSpan :: Position -> SrcSpan -> Bool
 p `isInsideSrcSpan` r = sp <= p && p <= ep
