aad98a8b0cf4eb19017c5462e597e9dc34232a70
Matthew Pickering <matthewtpickering@gmail.com>
2020-05-09 09:08:54 +0100

    Allow a failed cradle to report diagnostics and implement none cradle
    The rendering of the error could be better but it's still a good
    improvement.

--- src/Development/IDE/Core/Rules.hs
+++ src/Development/IDE/Core/Rules.hs
@@ -589,7 +589,7 @@ instance Hashable GhcSessionIO
 instance NFData   GhcSessionIO
 instance Binary   GhcSessionIO
 
-newtype GhcSessionFun = GhcSessionFun (FilePath -> Action HscEnvEq)
+newtype GhcSessionFun = GhcSessionFun (FilePath -> Action (IdeResult HscEnvEq))
 instance Show GhcSessionFun where show _ = "GhcSessionFun"
 instance NFData GhcSessionFun where rnf !_ = ()
 
@@ -607,9 +607,10 @@ loadGhcSession = do
         GhcSessionFun fun <- useNoFile_ GhcSessionIO
         alwaysRerun
         val <- fun $ fromNormalizedFilePath file
+
         -- TODO: What was this doing before?
 --        opts <- getIdeOptions
-        return (Just (BS.pack $ show $ hash val), ([], Just val))
+        return (Just (BS.pack (show ( hash (snd val)))), val)
 
 getHiFileRule :: Rules ()
 getHiFileRule = defineEarlyCutoff $ \GetHiFile f -> do
